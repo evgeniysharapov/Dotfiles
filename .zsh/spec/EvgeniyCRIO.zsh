8d86f2dd54af445bff33399b1200e342c0d1abfb
Evgeniy Sharapov <evgeniy.sharapov@clinicalresearch.io>
2023-10-27 16:04:10 -0400

    using kube-ps1 in a prompt

--- .zsh/spec/EvgeniyCRIO.zsh
+++ .zsh/spec/EvgeniyCRIO.zsh
@@ -12,6 +12,12 @@ source $HOME/google-cloud-sdk/path.zsh.inc
 # kubectl completions
 source <(kubectl completion zsh)
 
+# brew install kube-ps1
+if [[ -f "/opt/homebrew/opt/kube-ps1/share/kube-ps1.sh" ]]; then
+    source "/opt/homebrew/opt/kube-ps1/share/kube-ps1.sh"
+    PROMPT='$(kube_ps1)'$PROMPT # or RPROMPT='$(kube_ps1)'
+fi
+    
 # For Google Cloud SDK development 
 export GOOGLE_APPLICATION_CREDENTIALS=/Users/evgeniysharapov/.keys/crio-dev-267118-fb2d927062e3.json
 

e873c7ab48555683394f218fa6b481335c43cf9a
Evgeniy Sharapov <evgeniy.sharapov@clinicalresearch.io>
2023-10-27 15:40:51 -0400

    added kubectl completions

--- .zsh/spec/EvgeniyCRIO.zsh
+++ .zsh/spec/EvgeniyCRIO.zsh
@@ -9,6 +9,9 @@ cdpath=($HOME/Documents/EDC $HOME/Documents $HOME/Documents/Looker)
 source $HOME/google-cloud-sdk/completion.zsh.inc
 source $HOME/google-cloud-sdk/path.zsh.inc
 
+# kubectl completions
+source <(kubectl completion zsh)
+
 # For Google Cloud SDK development 
 export GOOGLE_APPLICATION_CREDENTIALS=/Users/evgeniysharapov/.keys/crio-dev-267118-fb2d927062e3.json
 

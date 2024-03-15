7b5d9509ed4338b4cb32a3151164e1aa40324e85
Evgeniy Sharapov <sharape@labcorp.com>
2024-03-15 12:08:46 -0400

    updated autocd, gitconfig

--- .zsh/spec/5CG3312VRG.zsh
+++ .zsh/spec/5CG3312VRG.zsh
@@ -3,7 +3,17 @@
 # 
 
 setopt auto_cd
-cdpath=($HOME/Projects /mnt/c/Users/sharape /mnt/c/Users/sharape/Documents)
+cdpath=($HOME/Projects $HOME/Personal /mnt/c/Users/sharape /mnt/c/Users/sharape/Documents)
+
+# pyenv (installed according to https://kfields.me/blog/pyenv_on_ubuntu_22
+export PATH="$HOME/.pyenv/bin:$PATH"
+which pyenv > /dev/null && \
+    eval "$(pyenv init --path)" && eval "$(pyenv virtualenv-init -)"
+
+# Adding nodenv, see https://gist.github.com/mrbar42/faa10a68e32a40c2363aed5e150d68da
+which nodenv > /dev/null && \
+    eval "$(nodenv init - zsh)"
+
 
 # added Intellij IDEA
 export PATH=/opt/idea-IC-233.14015.106/bin:$PATH
